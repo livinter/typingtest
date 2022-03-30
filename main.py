90b4b2eab580cef61452ee06f670b26661f214b8
NOP <livint@posteo.de>
2022-03-30 13:57:28 +0200

    fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import time
-import numpy as np
 import pandas as pd
 import sys
 from kbhit import KBHit
 from find_next_letters import get_next_letters
 from utils import load_config, read_random_text_part, linear_regression_get_beta, write_event, load_events
+from datetime import datetime, timezone, timedelta
 
 config = load_config()
 kb = KBHit()
 ESC = chr(27)
 ENTER = chr(10)
 BACK = chr(127)
+time_zone = datetime.utcnow().astimezone().tzinfo.utcoffset(datetime.now()).seconds
 
 
 def text_event():
@@ -35,12 +36,12 @@ def text_event():
                 print(end=k_in, flush=True)
 
     return {"start_time": start_time,
+            "time_zone": time_zone,
             "type": "text",
             "text": text}
 
 
-def typing_errors(time_passed:list, example_text:str):
-    print(time_passed, example_text)
+def typing_errors(time_passed: list, example_text: str):
     print(end="Analyzing typing errors...", flush=True)
     written_text = ""
     time_passed_with_error = []
@@ -57,11 +58,13 @@ def typing_errors(time_passed:list, example_text:str):
 
 def typing_test():
     time_passed = []
-    example_text = read_random_text_part(config["character_count"],
-                                         convert_german_letters=config["convert_german_letters"])
+    example_text = read_random_text_part(character_count=config["character_count"],
+                                         convert_letters=config["convert_letters"],
+                                         language=config["language"])
+
     print("Typing test, please write the text below, and finish with ESC")
     print(example_text)
-    print("-"*30)
+    print("-" * 30)
     start_time = int(time.time())
     last_hit = time.time()
 
@@ -79,16 +82,19 @@ def typing_test():
                 print(end=k_in, flush=True)
             last_hit = time.time()
         time.sleep(0.0001)
+
     return {"start_time": start_time,
+            "time_zone": time_zone,
+            "language": config["language"],
             "type": "typing_test",
             "time_passed": typing_errors(time_passed, example_text),
             "example_text": example_text}
 
 
 def analyze_typing_test(typing_test_data):
-    d={"time":[t for t,l,e in typing_test_data["time_passed"]],
-        "letter":[l for t,l,e in typing_test_data["time_passed"]],
-        "correct":[e for t,l,e in typing_test_data["time_passed"]]}
+    d = {"time": [t for t, l, e in typing_test_data["time_passed"]],
+         "letter": [l for t, l, e in typing_test_data["time_passed"]],
+         "correct": [e for t, l, e in typing_test_data["time_passed"]]}
     df = pd.DataFrame(d)
 
     df["left"] = df["letter"].isin([e for e in config["left_hand"]])
@@ -101,19 +107,21 @@ def analyze_typing_test(typing_test_data):
                "2x left hand ": df[df["left_left"]],
                "2x right hand": df[df["right_right"]],
                }
-    print(f"typing speed:  ({len(df)} character_count)")
+    print(f"typing speed:  ({len(df)} characters)")
     for k, v in metrics.items():
-        verror=v[v["correct"]==False]["time"]
+        verror = v[v["correct"] == False]["time"]
         speed_up = linear_regression_get_beta(v["time"])
-        vt=v["time"]
+        vt = v["time"]
         print(
-            f"            {k} : median: {vt.median():.2f}s "+
-            f"trend: {int(speed_up * 100 * len(vt))}%  "+
+            f"            {k} : median: {vt.median():.2f}s " +
+            f"trend: {int(speed_up * 100 * len(vt))}%  " +
             f"error: {int(10000 * len(verror) / len(v)) / 100}% ({len(verror)})")
-from datetime import datetime
+
 
 def print_event(event):
-    print(end=f"\nEvent from {datetime.fromtimestamp(event['start_time'])}   ")
+    tz = timezone(timedelta(seconds=event['time_zone']))
+    stime = datetime.fromtimestamp(event['start_time'], tz=tz)
+    print(end=f"\nEvent from {stime}   ")
     if event["type"] == "typing_test":
         analyze_typing_test(event)
     if event["type"] == "text":
@@ -123,12 +131,12 @@ def print_event(event):
 if __name__ == '__main__':
     while True:
         print(
-"""select an option:
-   1) register an event
-   2) measure typing speed 
-   3) show history    
- ESC) quit                 
-""", flush=True)
+            """select an option:
+               1) register an event
+               2) measure typing speed 
+               3) show history    
+             ESC) quit                 
+            """, flush=True)
         while not kb.kbhit():
             time.sleep(0.001)
 
--- utils.py
+++ utils.py
@@ -4,22 +4,25 @@ import numpy as np
 import yaml
 import json
 import os
+from unidecode import unidecode
 
 
-def read_random_text_part(character_count, convert_german_letters=True):
-    text_files = list(glob.glob("texts/*.txt"))
+def read_random_text_part(character_count, convert_letters=True, language=None):
+    if language:
+        language_filter = "." + language
+    else:
+        language_filter = ""
+    text_files = list(glob.glob(f"texts/*{language_filter}.txt"))
     text_file = text_files[random.randint(0, len(text_files) - 1)]
     text_data = open(text_file).read()
-    if convert_german_letters:
-        text_data = text_data.replace("ö", "oe").replace("«", "").replace("ß", "ss").replace("»", "").replace("ä",
-                                                                                                              "ae").replace(
-            "ü", "ue")
+    if convert_letters:
+        text_data = unidecode(text_data)
     text_data = text_data * (1 + character_count // len(text_data))
     random_pos = random.randint(0, len(text_data) - 1) - character_count
     return text_data[random_pos:random_pos + character_count]
 
 
-# Als Formel: β = ∑ [(xi - ∅x) × (yi - ∅y)] / ∑(xi - ∅x)2
+#  β = ∑ [(xi - ∅x) × (yi - ∅y)] / ∑(xi - ∅x)2
 def linear_regression_get_beta(y):
     x = np.arange(len(y))
     y = np.array(y)
@@ -30,18 +33,18 @@ def load_config():
     with open(r'config.yaml') as file:
         return yaml.load(file, Loader=yaml.SafeLoader)
 
+
 def write_event(event):
     os.makedirs("results", exist_ok=True)
-    with open(f'results/{event["start_time"]}.json',"wt") as file:
+    with open(f'results/{event["start_time"]}.json', "wt") as file:
         print("writing event...")
         return json.dump(event, file)
 
+
 def load_events():
     print("loading events...")
-    events=[]
-    for event_file in glob.glob("results/*.json"):
+    events = []
+    for event_file in sorted(list(glob.glob("results/*.json"))):
         with open(event_file) as file:
-            events.append( json.load(file))
+            events.append(json.load(file))
     return events
-
-
